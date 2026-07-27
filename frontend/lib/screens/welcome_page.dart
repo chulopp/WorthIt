9db64190e948f0386970445c3a8b9cbf459dc4ad
Fallah <fallahiqbal@gmail.com>
2026-07-27 18:34:50 +0700

    style: slightly raise google sign in button and guest link position on welcome page

--- frontend/lib/screens/welcome_page.dart
+++ frontend/lib/screens/welcome_page.dart
@@ -182,7 +182,7 @@ class _WelcomePageState extends State<WelcomePage> {
                     ),
                   ),
 
-                  const SizedBox(height: 28),
+                  const SizedBox(height: 52),
                 ],
               ),
             ),
